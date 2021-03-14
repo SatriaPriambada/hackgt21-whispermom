92b0bf09cd791411c554d83aa4d4de7786d535e2
Satria Priambada <spriambada3@gatech.edu>
2021-03-14 01:59:26 -0500

    add demo

---
+++ README.md
@@ -0,0 +1,67 @@
+# WhisperMom
+HealthHackGT'21
+An app that let's you see any User's Status in realtime
+[Base project](https://github.com/mappmechanic/whats-up-realtime-status-update)
+[View tutorial](https://pusher.com/tutorials/social-network-javascript/)
+
+## Prerequisite Softwares
+- NodeJS
+- Yarn (Optional)
+
+## Running the Project
+In order to run the app on your machines, please follow the below given steps:
+
+1. Run either of the following commands to install dependencies
+
+```
+ npm install
+```
+
+OR 
+
+```
+ yarn
+```
+
+2. If you just want to use the app run in command line and open localhost:9000 on web browser
+```
+yarn start
+```
+ELSE
+3. Signup at [https://pusher.com/signup](https://pusher.com/signup). 
+
+4. Create a new app to obtain the API Key, secret & appId. 
+
+Replace the respective key, secret & appId for pusher initialisation in **server.js** file with your values:
+
+```javascript
+    var pusher = new Pusher({
+    appId: '<your-app-id>',
+    key: '<your-api-key>',
+    secret: '<your-app-secret>',
+    encrypted: true
+    });
+```
+
+5. Finally you will have to also replace your app-key in **app.js** file too:
+
+```javascript
+ ...
+ pusher = new Pusher('<your-api-key>', {
+    authEndpoint: '/usersystem/auth',
+    encrypted: true
+ }),
+ ...
+```
+
+6. Now we are ready to run our app using the following node commands
+
+```
+node server
+```
+
+7. We will be able to access the app at [http://localhost:9000](http://localhost:9000)
+
+For Any clarifications or questions Tweet to me at 
+[https://twitter.com/mappmechanic](https://twitter.com/mappmechanic)
+
---
+++ server.js
@@ -0,0 +1,93 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+
+
+var Pusher = require('pusher');
+
+const pusher = new Pusher({
+  appId: "1171525",
+  key: "9e7dda7ab5cb3b46a850",
+  secret: "fce43e87f1630c3c9c1d",
+  cluster: "us2",
+  useTLS: true,
+  encrypted: true
+});
+
+var app = express();
+
+// must use cookieParser before expressSession
+app.use(cookieParser());
+
+app.use(expressSession({secret:'fce43e87f1630c3c9c1d',resave: true, saveUninitialized: true}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/register', function(req, res){
+  console.log(req.body);
+  if(req.body.username && req.body.status){
+    var newMember = {
+      username: req.body.username,
+      status: req.body.status
+    }
+    req.session.user = newMember;
+    res.json({  
+      success: true,
+      error: false
+    });
+  }else{
+    res.json({  
+      success: false,
+      error: true,
+      message: 'Incomplete information: username and status are required'
+    });
+  }
+});
+
+app.post('/usersystem/auth', function(req, res) {
+  var socketId = req.body.socket_id;
+  var channel = req.body.channel_name;
+  var currentMember = req.session.user;
+  var presenceData = {
+    user_id: currentMember.username,
+    user_info: {
+      status: currentMember.status,
+    }
+  };
+  var auth = pusher.authenticate(socketId, channel, presenceData);
+  res.send(auth);
+});
+
+app.get('/isLoggedIn', function(req,res){
+  if(req.session.user){
+    res.send({ 
+      authenticated: true 
+    });
+  }else{
+    res.send({ authenticated: false });
+  }
+});
+
+app.get('/logout', function(req,res){
+  if(req.session.user){
+    req.session.user = null;
+  }
+  res.redirect('/');
+});
+
+// Error Handler for 404 Pages
+app.use(function(req, res, next) {
+    var error404 = new Error('Route Not Found');
+    error404.status = 404;
+    next(error404);
+});
+
+module.exports = app;
+
+app.listen(9000, function(){
+  console.log('Example app listening on port 9000!')
+});
